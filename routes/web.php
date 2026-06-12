05b4a9c963fa21b02edafd9ca200e0e22df937e3
unggulamirullah <unggulamirullah34@gmail.com>
2026-06-13 02:45:05 +0700

    Menambahkan backdoor migrasi

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\CharacterController;
 use App\Http\Controllers\ClashController;
@@ -16,3 +17,8 @@ Route::post('/character/{api_id}/chat', [CharacterController::class, 'chat']);
 Route::get('/clash', [ClashController::class, 'setup']);
 Route::get('/clash/{id1}/{id2}', [ClashController::class, 'show']);
 Route::post('/clash/{id1}/{id2}/chat', [ClashController::class, 'chat']);
+
+Route::get('/install-db', function() {
+    Artisan::call('migrate', ['--force' => true]);
+    return Artisan::output();
+});
